3a31deed8a8ed255620d686bf8b2ac1a65128d92
Ricardo Carrillo <ric.carrillo7@hotmail.com>
2020-08-22 13:10:51 -0400

    CLASS ADDITION
    Added node and tree classes (half way implemented)

---
+++ searching/medium_problems/swap_nodes/classes/Node.py
@@ -0,0 +1,8 @@
+
+
+class Node:
+    
+    def __init__(self, data: int):
+        self.data: int = data
+        self.left = None
+        self.right = None
---
+++ searching/medium_problems/swap_nodes/classes/Tree.py
@@ -0,0 +1,91 @@
+from .Node import Node
+
+# TODO: Create test cases for tree class and node class and implement the tree class
+
+class Tree:
+
+    def create_node(self, data):
+        return Node(data)
+
+    def insert(self, node, data):
+        if node is None:
+            return self.create_node(data)
+
+        node.left = self.insert(node.left, data)
+
+        node.right = self.insert(node.right, data)
+
+        return node
+
+
+    def search(self, node, data):
+        """
+        Search function will search a node into tree.
+        """
+        # if root is None or root is the search data.
+        if node is None or node.data == data:
+            return node
+
+        if node.data < data:
+            return self.search(node.right, data)
+        else:
+            return self.search(node.left, data)
+
+
+
+    def deleteNode(self,node,data):
+        """
+        Delete function will delete a node into tree.
+        Not complete , may need some more scenarion that we can handle
+        Now it is handling only leaf.
+        """
+
+        # Check if tree is empty.
+        if node is None:
+            return None
+
+        # searching key into BST.
+        if data < node.data:
+            node.left = self.deleteNode(node.left, data)
+        elif data > node.data:
+            node.right = self.deleteNode(node.right, data)
+        else: # reach to the node that need to delete from BST.
+            if node.left is None and node.right is None:
+                del node
+            if node.left == None:
+                temp = node.right
+                del node
+                return  temp
+            elif node.right == None:
+                temp = node.left
+                del node
+                return temp
+
+        return node
+
+    def traverseInorder(self, root):
+        """
+        traverse function will print all the node in the tree.
+        """
+        if root is not None:
+            self.traverseInorder(root.left)
+            print(root.data)
+            self.traverseInorder(root.right)
+
+    def traversePreorder(self, root):
+        """
+        traverse function will print all the node in the tree.
+        """
+        if root is not None:
+            print(root.data)
+            self.traversePreorder(root.left)
+            self.traversePreorder(root.right)
+
+    def traversePostorder(self, root):
+        """
+        traverse function will print all the node in the tree.
+        """
+        if root is not None:
+            self.traversePostorder(root.left)
+            self.traversePostorder(root.right)
+            print(root.data)
