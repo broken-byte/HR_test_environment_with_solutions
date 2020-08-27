af454718310eb29aaafe3670cad304718088ffbc
Ricardo Carrillo <ric.carrillo7@hotmail.com>
2020-08-27 12:54:46 -0400

    IMPLEMENTATION ADDITIONS
    Added further implementation to tree along with tests to confirm functionaity along the way

--- searching/medium_problems/swap_nodes/classes/Tree.py
+++ searching/medium_problems/swap_nodes/classes/Tree.py
@@ -1,36 +1,58 @@
 from math import log
 
 from searching.medium_problems.swap_nodes.classes.Node import Node
-# TODO: Create test cases for tree class and node class and implement the tree class
-# TODO: Try Iterative approach
-    #TODO: fix partitioner of indices
+# TODO: Change level constructor so that it appends sequentially, not in pairs, since everything is in order
+# TODO: Connect levels
+# TODO: Create Swap function
 
 
 class Tree:
 
     def __init__(self):
         self.root = Node(1)
+        self.indices: list = []
+        self.partitioned_indices: list = []
         self.tree_levels: list = []
-        self.leveled_indices: list = []
         self.traversal_path: list = []
 
     def construct_with(self, indices: list):
-        self.partition_indices_into_levels(indices)
+        self.indices = indices
+        self.partition_indices_into_levels()
+        self.construct_tree_levels_with_partitioned_indices()
+        self.connect_levels()
 
-    def partition_indices_into_levels(self, indices: list):
-        indices_length: int = len(indices)
+    def partition_indices_into_levels(self):
+        indices_length: int = len(self.indices)
         number_of_tree_levels: int = int(log(indices_length + 1, 2))
-        partitioned_indices: list = []
-        for level in range(number_of_tree_levels):
-            start_index: int = 2**level - 1
-            size: int = 2**level
-            end_index: int = start_index + size
-            partition: list = indices[start_index: end_index]
-            partitioned_indices.append(partition)
-        self.leveled_indices = partitioned_indices
+        for level_number in range(number_of_tree_levels):
+            partition = self.construct_partition_with(level_number)
+            self.partitioned_indices.append(partition)
 
-    def construct_tree_levels_with(self, leveled_indices: list):
+    def construct_tree_levels_with_partitioned_indices(self):
+        self.tree_levels.append([self.root])
+        for indices_partition in self.partitioned_indices:
+            nodes: list = self.construct_nodes_with(indices_partition)
+            self.tree_levels.append(nodes)
+
+    def connect_levels(self):
         pass
+
+    def construct_partition_with(self, level_number: int) -> list:
+        start_index: int = 2 ** level_number - 1
+        size: int = 2 ** level_number
+        end_index: int = start_index + size
+        partition: list = self.indices[start_index: end_index]
+        return partition
+
+    @staticmethod
+    def construct_nodes_with(indices_partition) -> list:
+        nodes: list = []
+        for index_pair in indices_partition:
+            left = Node(index_pair[0]) if index_pair[0] != -1 else None
+            right = Node(index_pair[1]) if index_pair[1] != -1 else None
+            node_pair = left, right
+            nodes.append(node_pair)
+        return nodes
 
     def get_in_order_traversal(self) -> list:
         self.traversal_path.clear()
--- searching/medium_problems/swap_nodes/tests/test_tree.py
+++ searching/medium_problems/swap_nodes/tests/test_tree.py
@@ -7,6 +7,7 @@ from searching.medium_problems.swap_nodes.classes.Tree import Tree
 class TreeTester(TestCase):
 
     node_checker_path: list = []
+    tree_levels: list = []
 
     def test_tree_should_have_a_root_with_data_of_1(self):
         tree: Tree = Tree()
@@ -22,18 +23,68 @@ class TreeTester(TestCase):
 
     def test_that_tree_can_partition_indices_into_levels(self):
         tree: Tree = Tree()
-        indices: list = [
-            [2, 3],
-            [-1, -1],
-            [-1, -1]
-        ]
-        tree.partition_indices_into_levels(indices)
+        indices: list = [[2, 3], [-1, -1], [-1, -1]]
+        tree.indices = indices
+        tree.partition_indices_into_levels()
+
         expected: list = [
             [[2, 3]],
             [[-1, -1], [-1, -1]]
         ]
-        actual: list = tree.leveled_indices
+        actual: list = tree.partitioned_indices
         self.assertEqual(expected, actual)
+
+    def test_that_tree_can_construct_a_size_1_partition_with_a_level_number(self):
+        tree: Tree = Tree()
+        indices: list = [[2, 3], [-1, -1], [-1, -1]]
+        tree.indices = indices
+
+        expected: list = [[2, 3]]
+        actual: list = tree.construct_partition_with(level_number=0)
+        self.assertEqual(expected, actual)
+
+    def test_that_tree_can_construct_a_size_2_partition_with_a_level_number(self):
+        tree: Tree = Tree()
+        indices: list = [[2, 3], [-1, -1], [-1, -1]]
+        tree.indices = indices
+
+        expected: list = [[-1, -1], [-1, -1]]
+        actual: list = tree.construct_partition_with(level_number=1)
+        self.assertEqual(expected, actual)
+
+    def test_that_tree_can_construct_nodes_with_partition_of_non_null_data(self):
+        tree: Tree = Tree()
+        partition: list = [[2, 3]]
+        expected: list = [(Node(2).data, Node(3).data)]
+        actual_nodes: list = tree.construct_nodes_with(partition)
+        actual_unpacked_nodes: list = [(pair[0].data, pair[1].data) for pair in actual_nodes]
+        self.assertEqual(expected, actual_unpacked_nodes)
+
+    def test_that_tree_can_construct_null_nodes_with_partition_of_null_data(self):
+        tree: Tree = Tree()
+        partition: list = [[-1, -1], [-1, -1]]
+        expected: list = [(None, None), (None, None)]
+        actual: list = tree.construct_nodes_with(partition)
+        self.assertEqual(expected, actual)
+
+    def test_that_tree_can_construct_tree_levels_with_partitioned_indices(self):
+        tree: Tree = Tree()
+        indices: list = [[2, 3], [-1, -1], [-1, -1]]
+        tree.indices = indices
+        tree.partition_indices_into_levels()
+        tree.construct_tree_levels_with_partitioned_indices()
+
+        expected: list = [
+            [Node(1).data],
+            [(Node(2).data, Node(3).data)],
+            [(None, None), (None, None)]
+        ]
+        self.tree_levels.clear()
+        actual_tree_levels: list = tree.tree_levels
+        self.tree_levels = actual_tree_levels
+        # TODO: Rewrite node level construction be sequentially ordered, instead of in pairs
+        # self.assertEqual(expected, actual_unpacked_tree_levels)
+
 
     def test_that_tree_can_construct_given_indices(self):
         self.node_checker_path.clear()
