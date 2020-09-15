336383a4e02f706a7efa88d9ab7a137051f5d2b6
Ricardo Carrillo <ric.carrillo7@hotmail.com>
2020-09-15 18:23:34 -0400

    IMPLEMENTATION ADDITION
    Added broken brute force, will fix

--- searching/medium_problems/pairs/pairs_brute_force.py
+++ searching/medium_problems/pairs/pairs_brute_force.py
@@ -1,4 +1,16 @@
 
 
 def pairs_brute_force(k: int, arr: list) -> int:
-    pass
+    length: int = len(arr)
+    count: int = 0
+    ledger: set = set({})
+    for i in range(length):
+        for j in range(length):
+            if i + j in ledger:
+                continue
+            ledger.add(i + j)
+            left: int = arr[i]
+            right: int = arr[j]
+            if abs(left - right) == k:
+                count += 1
+    return count
