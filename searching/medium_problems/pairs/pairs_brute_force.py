9daaf57d27a4ae2d38b4aad53123dfe3b4403f13
Ricardo Carrillo <ric.carrillo7@hotmail.com>
2020-09-16 17:21:49 -0400

    BRUTE FORCE PASSING ON FUNCTIONALITY
    Fixed brute force approach, fails on time complexity at O(n^2) speed

--- searching/medium_problems/pairs/pairs_brute_force.py
+++ searching/medium_problems/pairs/pairs_brute_force.py
@@ -1,16 +1,17 @@
 
-
+# TODO: Optimize
 def pairs_brute_force(k: int, arr: list) -> int:
     length: int = len(arr)
     count: int = 0
     ledger: set = set({})
     for i in range(length):
         for j in range(length):
-            if i + j in ledger:
-                continue
-            ledger.add(i + j)
             left: int = arr[i]
             right: int = arr[j]
+            pair: tuple = tuple(sorted([str(left), str(right)]))
+            if pair in ledger:
+                continue
+            ledger.add(pair)
             if abs(left - right) == k:
                 count += 1
     return count
