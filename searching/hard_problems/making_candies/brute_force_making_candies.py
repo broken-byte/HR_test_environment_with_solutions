beee698c0cc0d22713464ca5680412d3bf8eeda5
Ricardo Carrillo <ric.carrillo7@hotmail.com>
2020-10-12 13:01:55 -0400

    BRAINSTORMING
    Added the code of my brainstorm session

--- searching/hard_problems/making_candies/brute_force_making_candies.py
+++ searching/hard_problems/making_candies/brute_force_making_candies.py
@@ -3,3 +3,27 @@
 def brute_force_making_candies(m: int, w: int, p: int, n: int) -> int:
     pass
 
+
+class CandyFactory:
+
+    def __init__(self, machines: int, workers: int, price_per_unit: int, goal: int):
+        self.machines: int = machines
+        self.workers: int = workers
+        self.price_per_worker: int = price_per_unit
+        self.price_per_machine: int = price_per_unit
+        self.goal: int = goal
+        self.candies: int = 0
+
+    def minimum_time_to_meet_goal(self):
+        pass
+
+    def make_candies(self):
+        self.candies += self.machines * self.candies
+
+    def buy_machines(self, amount: int, price: int):
+        self.candies -= price * amount
+        self.machines += amount
+
+    def hire_workers(self, amount: int, price: int):
+        self.candies -= price * amount
+        self.workers += amount
