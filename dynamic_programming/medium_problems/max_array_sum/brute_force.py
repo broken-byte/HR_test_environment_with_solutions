2b405f7993724795e48098268a496a9caaade526
Ricardo Carrillo <ric.carrillo7@hotmail.com>
2020-11-10 15:22:36 -0500

    FAILURE
    Ran out of time, will analyze HR user submission and create my solution from there

--- dynamic_programming/medium_problems/max_array_sum/brute_force.py
+++ dynamic_programming/medium_problems/max_array_sum/brute_force.py
@@ -1,30 +1,61 @@
+import logging
+
+
+logging.basicConfig(level=logging.DEBUG)
 
 
 def max_subset_sum(arr: list) -> int:
-    sub_set_generator: SubsetGenerator = SubsetGenerator(arr)
-    return sub_set_generator.get_max_sub_set_sum()
+    dp = {}  # key : max index of subarray, value = sum
+    dp[0], dp[1] = arr[0], max(arr[0], arr[1])
+    for i, num in enumerate(arr[2:], start=2):
+        logging.debug(f"dynamic programming dp: {dp}")
+        dp[i] = max(dp[i - 1], dp[i - 2] + num, dp[i - 2], num)
+    return dp[len(arr) - 1]
 
-# TODO: Only noncontigious subsets are valid for sum comparison, need to change alg to account for thats
+# TODO: Analyze the above solution and make your own
 
-class SubsetGenerator:
+
+class SubSequenceGenerator:
     def __init__(self, array: list):
-        self.memo: set = set()
         self.array: list = array
-        self.sub_sets: list = []
-        self.current_max_sum: int = 0
+        self.non_adjacent_sub_sequences: list = []
+        self.sub_sequences: list = []
 
-    def get_max_sub_set_sum(self):
-        self.recursively_look_for_max_sub_set_sum(0)
-        return self.current_max_sum
+    def get_all_non_adjacent_sub_sequences(self) -> list:
+        self.recursively_build_non_adjacent_sub_sequences([], -1, 0)
+        return self.non_adjacent_sub_sequences
 
-    def recursively_look_for_max_sub_set_sum(self, summation: int, index=0):
-        if index == len(self.array):
-            return
+    def get_all_sub_sequences(self):
+        self.recursively_build_sub_sequences([], 0)
+        return self.sub_sequences
+
+    def recursively_build_non_adjacent_sub_sequences(self, sub: list, previously_added_i: int, current_i: int):
+        if current_i == len(self.array):
+            if len(sub) != 0:
+                self.sub_sequences.append(sub)
         else:
-            self.recursively_look_for_max_sub_set_sum(summation, index + 1)
-            self.recursively_look_for_max_sub_set_sum(summation + self.array[index], index + 1)
+            # traverse without adding
+            self.recursively_build_non_adjacent_sub_sequences(sub, previously_added_i, current_i + 1)
+
+            if current_i == 0:  # Add first time only
+                self.recursively_build_non_adjacent_sub_sequences(sub + [self.array[current_i]], current_i,
+                                                                  current_i + 1)
+            elif current_i - previously_added_i > 1:  # Add if difference is greater than 1
+                self.recursively_build_non_adjacent_sub_sequences(sub + [self.array[current_i]], current_i,
+                                                                  current_i + 1)
+            else:  # traverse without adding
+                self.recursively_build_non_adjacent_sub_sequences(sub, current_i, current_i + 1)
+
+    def recursively_build_sub_sequences(self, sub: list, current_i):
+        if current_i == len(self.array):
+            if len(sub) != 0:
+                self.sub_sequences.append(sub)
+        else:
+            # Omit
+            self.recursively_build_sub_sequences(sub, current_i + 1)
+            # Add
+            self.recursively_build_sub_sequences(sub + [self.array[current_i]], current_i + 1)
 
 
 if __name__ == '__main__':
-    gen: SubsetGenerator = SubsetGenerator([1, 2, 3])
-    print(gen.get_max_sub_set_sum())
+    max_subset_sum([-2, 1, 3])
