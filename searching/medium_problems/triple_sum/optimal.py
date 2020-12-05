ebfc490688fedf0776515de05d5a76327b31034c
Ricardo Carrillo <ric.carrillo7@hotmail.com>
2020-12-05 12:41:34 -0500

    REFACTOR
    Refactored triple sum directory to the new standard

--- searching/medium_problems/triple_sum/optimal.py
+++ searching/medium_problems/triple_sum/optimal.py
@@ -1,5 +1,14 @@
 import bisect
 
+from test_utilities.dynamic_test_creator import dynamically_generate_tests, run_dynamic_tests
+from searching.medium_problems.triple_sum.test_resources.functionality_test_data import functionality_test_data
+
+
+def triple_sum(a: list, b: list, c: list) -> int:  # O(b*{log(a) + log(c)})
+    triplet_counter = TripletCounter(a, b, c)
+    triplet_count: int = triplet_counter.count_triplets()
+    return triplet_count
+
 
 class TripletCounter:
 
@@ -46,4 +55,9 @@ class TripletCounter:
 
     @staticmethod
     def triplets_are_possible_given(valid_elements_in_a: int, valid_elements_in_c: int) -> bool:
+
         return valid_elements_in_a != 0 and valid_elements_in_c != 0
+
+
+if __name__ == '__main__':
+    pass
