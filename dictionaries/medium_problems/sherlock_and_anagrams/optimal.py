c998e59e70ad2c2956823e6ccc3661e189799d4f
Ricardo Carrillo <ric.carrillo7@hotmail.com>
2021-04-08 11:01:19 -0400

    Refactored sherlock and anagrams problem

--- dictionaries/medium_problems/sherlock_and_anagrams/optimal.py
+++ dictionaries/medium_problems/sherlock_and_anagrams/optimal.py
@@ -1,24 +1,28 @@
-  
-def getAllAnagramPairsIn(s):
+from test_utilities.dynamic_test_creator import dynamically_generate_tests, run_dynamic_tests
+from dictionaries.medium_problems.sherlock_and_anagrams.test_resources.functionality_test_data import \
+    functionality_test_data
+
+
+def optimal(s: list) -> int:
     n = len(s)
-    numberOfAnagramPairs = 0
+    number_of_anagram_pairs = 0
 
-    for i in range(1, n): # length of the sublist 
-        subStringsWithCounts = {}
+    for i in range(1, n):  # length of the sublist
+        sub_strings_with_counts = {}
 
-        for j in range(n - i + 1): # Number of sublists in this iteration
-            subString = s[j: j + i]
-            sortedSubList = sorted(list(subString))
+        for j in range(n - i + 1):  # Number of sub lists in this iteration
+            sub_string = s[j: j + i]
+            sorted_sub_list = sorted(list(sub_string))
 
-            sortedSubString = "".join(sortedSubList) # Convert the sortedSubList back into a string
+            sorted_sub_string = "".join(sorted_sub_list)  # Convert the sortedSubList back into a string
 
-            if sortedSubString not in subStringsWithCounts: # There hasn't been a match of the sortedSubString
-                subStringsWithCounts[sortedSubString] = 1
+            if sorted_sub_string not in sub_strings_with_counts:  # There hasn't been a match of the sortedSubString
+                sub_strings_with_counts[sorted_sub_string] = 1
 
-            else: # There is a match, add to the number of instances of that sortedSubString
-                subStringsWithCounts[sortedSubString] += 1
+            else:  # There is a match, add to the number of instances of that sortedSubString
+                sub_strings_with_counts[sorted_sub_string] += 1
 
-            numberOfAnagramPairs += subStringsWithCounts[sortedSubString] - 1
+            number_of_anagram_pairs += sub_strings_with_counts[sorted_sub_string] - 1
             '''
             Now, the above line of code might be confusing.
 
@@ -26,7 +30,7 @@ def getAllAnagramPairsIn(s):
 
             - Why increment by subStringsWithCounts[subString]?
 
-            - Why substract by 1?
+            - Why subtract by 1?
 
             After the conditional, subStringsWithCounts[substring] will
             always have AT LEAST 1, or in other words there is
@@ -49,20 +53,15 @@ def getAllAnagramPairsIn(s):
                 numberOfPairs = previousAmountOfPairs + (numberOfItems - 1)
 
             Voila! We have our calculation. This is why we increment 
-            numberOfAnagramPairs by subStringsWithCounts[subString] - 1!
+            number_of_anagram_pairs by subStringsWithCounts[subString] - 1!
             That is the direct reflection of: 
             numberOfPairs = previousAmountOfPairs + (numberOfItems - 1)
             In our code!
             '''
-    return numberOfAnagramPairs
-            
-def main():
+    return number_of_anagram_pairs
 
-    tests = ["mom", "abba", "abcd", "ifailuhkqq", "kkkk"]
 
-    for test in tests:
-        getAnagramCountsInString(test)
-        print("=============================================================================================\n")
+if __name__ == '__main__':
+    dynamically_generate_tests(functionality_test_data, optimal)
+    run_dynamic_tests()
 
-if __name__ == "__main__":
-    main()
---
+++ dictionaries/medium_problems/sherlock_and_anagrams/test_resources/functionality_test_data.py
@@ -0,0 +1,34 @@
+
+
+functionality_test_data: dict = {
+    "test_0": {
+        "params": {
+            "s": "mom"
+        },
+        "expected": 2
+    },
+    "test_1": {
+        "params": {
+            "s": "abba"
+        },
+        "expected": 4
+    },
+    "test_2": {
+        "params": {
+            "s": "abcd"
+        },
+        "expected": 0
+    },
+    "test_3": {
+        "params": {
+            "s": "ifailuhkqq"
+        },
+        "expected": 3
+    },
+    "test_4": {
+        "params": {
+            "s": "kkkk"
+        },
+        "expected": 10
+    }
+}
